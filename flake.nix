ae0bdeb14290025129bc4c70852ea1fd149a3b33
AndrewCouncil <andrew.p.council@gmail.com>
2025-02-01 22:12:28 -0500

    typo

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable;
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager;
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
