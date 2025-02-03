a13c49d81be3db28dcd9a912c92e4e1fa5c3bec4
AndrewCouncil <andrew.p.council@gmail.com>
2025-02-03 17:36:53 -0500

    only enable hyprland for now

--- modules/wm.nix
+++ modules/wm.nix
@@ -7,13 +7,13 @@
 }:
 
 {
-  # Enable Simple Desktop Display Manager
-  services.displayManager.sddm.enable = true;
+  # # Enable Simple Desktop Display Manager
+  # services.displayManager.sddm.enable = true;
 
   # Enable Hyprland
   programs.hyprland.enable = true;
 
-  # Enable KDE Plasma 6
-  services.desktopManager.plasma6.enable = true;
+  # # Enable KDE Plasma 6
+  # services.desktopManager.plasma6.enable = true;
 
 }
