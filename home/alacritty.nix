1037056c00fd29e386b80acf04aaeb859aa1f21f
AndrewCouncil <andrew.p.council@gmail.com>
2025-02-03 02:21:44 -0500

    formatting

--- home/alacritty.nix
+++ home/alacritty.nix
@@ -7,23 +7,23 @@ in
   programs.alacritty = {
     enable = true;
     settings = {
-        font = {
-          normal = {
-            family = font_family;
-            style = "Regular";
-          };
-          bold = {
-            family = font_family;
-            style = "Bold";
-          };
-          italic = {
-            family = font_family;
-            style = "Italic";
-          };
-          bold_italic = {
-            family = font_family;
-            style = "Bold Italic";
-          };
+      font = {
+        normal = {
+          family = font_family;
+          style = "Regular";
+        };
+        bold = {
+          family = font_family;
+          style = "Bold";
+        };
+        italic = {
+          family = font_family;
+          style = "Italic";
+        };
+        bold_italic = {
+          family = font_family;
+          style = "Bold Italic";
+        };
         size = 12;
       };
     };
