a3f2f20cc166198f0b05dc34488e1e2333856d30
AndrewCouncil <andrew.p.council@gmail.com>
2025-02-03 01:32:30 -0500

    try defaulting to kde, specialisation to hyprland

--- modules/wm.nix
+++ modules/wm.nix
@@ -1,4 +1,5 @@
 {
+  lib,
   config,
   pkgs,
   inputs,
@@ -6,16 +7,18 @@
 }:
 
 {
-  specialisation = {
-    kdewm.configuration = {
-      # Enable the KDE Plasma Desktop Environment.
-      services.displayManager.sddm.enable = true;
-      services.desktopManager.plasma6.enable = true;
-    };
+  services.displayManager.sddm.enable = lib.mkDefault true;
+  services.desktopManager.plasma6.enable = lib.mkDefault true;
 
+  specialisation = {
     hyprwm.configuration = {
+      # Disable KDE Plasma
+      services.displayManager.sddm.enable = lib.mkOverride false;
+      services.desktopManager.plasma6.enable = lib.mkOverride false;
+
       # Enable Hyprland
       programs.hyprland.enable = true;
     };
   };
 }
+
