7e212296a362828bb3a850dc5a7c352c0c3c3d26
AndrewCouncil <andrew.p.council@gmail.com>
2025-02-06 11:09:30 -0500

    add hyprland cachix

--- hosts/xps/configuration.nix
+++ hosts/xps/configuration.nix
@@ -23,6 +23,11 @@
     "flakes"
   ];
 
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
