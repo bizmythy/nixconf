164b29331ac9088811ad9ae6f20469f8fc267721
AndrewCouncil <andrew.p.council@gmail.com>
2025-02-06 16:29:59 -0500

    install 1password

--- hosts/xps/configuration.nix
+++ hosts/xps/configuration.nix
@@ -181,6 +181,14 @@
     ];
   };
 
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    # Certain features, including CLI integration and system authentication support,
+    # require enabling PolKit integration on some desktop environments (e.g. Plasma).
+    polkitPolicyOwners = [ "drew" ];
+  };
+
   # https://github.com/viperML/nh
   programs.nh = {
     enable = true;
