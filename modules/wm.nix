ed330abc01a0c9060a860d76bd88f21151b39528
AndrewCouncil <andrew.p.council@gmail.com>
2025-02-20 12:04:17 -0500

    working nvidia with double waybar...

--- modules/wm.nix
+++ modules/wm.nix
@@ -15,10 +15,12 @@
   programs.waybar = {
     enable = true;
   };
+  # Enable Simple Desktop Display Manager
+  services.displayManager.sddm.enable = true;
+  catppuccin.sddm.enable = false;
+  services.displayManager.sddm.wayland.enable = true;
 
   specialisation.kde.configuration = {
-    # Enable Simple Desktop Display Manager
-    services.displayManager.sddm.enable = true;
     # Enable KDE Plasma 6
     services.desktopManager.plasma6.enable = true;
   };
