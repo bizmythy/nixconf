474cc80432dcd205488049f2ccb8207d1ae623d1
AndrewCouncil <andrew.p.council@gmail.com>
2025-02-23 02:33:23 -0500

    enable nixld binary patching

--- modules/base.nix
+++ modules/base.nix
@@ -31,6 +31,14 @@
     trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
   };
 
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs; [
+      # Add any missing dynamic libraries for unpackaged
+      # programs here, NOT in environment.systemPackages
+    ];
+  };
+
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
