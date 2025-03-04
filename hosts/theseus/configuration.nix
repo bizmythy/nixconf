8f74fde8c58c393ee1581477b65073af75391a9b
AndrewCouncil <andrew.p.council@gmail.com>
2025-03-03 22:17:58 -0500

    add fwupd for framework

--- hosts/theseus/configuration.nix
+++ hosts/theseus/configuration.nix
@@ -14,6 +14,8 @@
 
   networking.hostName = "theseus";
 
+  services.fwupd.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
