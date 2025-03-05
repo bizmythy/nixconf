7bdff89c58a804e939076668ba394f2ed0d6e73c
AndrewCouncil <andrew.p.council@gmail.com>
2025-03-04 20:14:34 -0500

    correct firefox configuration with search and vertical tabs declared

--- home/firefox.nix
+++ home/firefox.nix
@@ -4,66 +4,35 @@
   ...
 }:
 
-let
-  lock-false = {
-    Value = false;
-    Status = "locked";
-  };
-  lock-true = {
-    Value = true;
-    Status = "locked";
-  };
-in
 {
-  programs = {
-    firefox = {
-      enable = true;
-      package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
-        extraPolicies = {
-          DisableTelemetry = true;
-          # add policies here...
+  programs.firefox = {
+    enable = true;
 
-          # /* ---- EXTENSIONS ---- */
-          # ExtensionSettings = {
-          #   "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
-          #   # uBlock Origin:
-          #   "uBlock0@raymondhill.net" = {
-          #     install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-          #     installation_mode = "force_installed";
-          #   };
-          #   # add extensions here...
-          # };
-
-          # ---- PREFERENCES ----
-          # Set preferences shared by all profiles.
-          Preferences = {
-            "browser.contentblocking.category" = {
-              Value = "strict";
-              Status = "locked";
-            };
-            "extensions.pocket.enabled" = lock-false;
-            "extensions.screenshots.disabled" = lock-true;
-            "sidebar.revamp" = lock-true;
-            "sidebar.verticalTabs" = lock-true;
-            # add global preferences here...
-          };
+    # https://nix-community.github.io/home-manager/options.html#opt-programs.firefox.profiles
+    profiles = {
+      profile_0 = {
+        # choose a profile name; directory is /home/<user>/.mozilla/firefox/profile_0
+        id = 0; # 0 is the default profile; see also option "isDefault"
+        name = "drew"; # name as listed in about:profiles
+        isDefault = true; # can be omitted; true if profile ID is 0
+        settings = {
+          # specify profile-specific preferences here; check about:config for options
+          "browser.newtabpage.activity-stream.feeds.section.highlights" = false;
+          "extensions.pocket.enabled" = false;
+          "sidebar.revamp" = true;
+          "sidebar.verticalTabs" = true;
         };
-      };
-
-      # ---- PROFILES ----
-      # Switch profiles via about:profiles page.
-      # For options that are available in Home-Manager see
-      # https://nix-community.github.io/home-manager/options.html#opt-programs.firefox.profiles
-      profiles = {
-        profile_0 = {
-          # choose a profile name; directory is /home/<user>/.mozilla/firefox/profile_0
-          id = 0; # 0 is the default profile; see also option "isDefault"
-          name = "drew"; # name as listed in about:profiles
-          isDefault = true; # can be omitted; true if profile ID is 0
-          settings = {
-            # specify profile-specific preferences here; check about:config for options
-            "browser.newtabpage.activity-stream.feeds.section.highlights" = false;
+        search = {
+          engines = {
+            "unduck" = {
+              urls = [ { template = "https://unduck.link?q={searchTerms}"; } ];
+              iconUpdateURL = "https://unduck.link/favicon.ico";
+              updateInterval = 24 * 60 * 60 * 1000; # every day
+            };
+            "Bing".metaData.hidden = true;
           };
+          default = "unduck";
+          privateDefault = "DuckDuckGo";
         };
       };
     };
