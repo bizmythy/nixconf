9980f9a464b4fbef62740b4760fcd2153e50d1cb
AndrewCouncil <andrew.p.council@gmail.com>
2025-03-06 00:11:15 -0500

    nice qtile-style workspace swapping! no script needed lol

--- home/wm/hypr/hyprland.nix
+++ home/wm/hypr/hyprland.nix
@@ -7,7 +7,7 @@
 
 {
   imports = [
-    ./waybar/waybar.nix
+    ../waybar/waybar.nix
   ];
 
   dconf = {
@@ -188,16 +188,16 @@
         "$mainMod, Tab, bringactivetotop" # bring it to the top
 
         # Switch workspaces with mainMod + [0-9]
-        "$mainMod, 1, workspace, 1"
-        "$mainMod, 2, workspace, 2"
-        "$mainMod, 3, workspace, 3"
-        "$mainMod, 4, workspace, 4"
-        "$mainMod, 5, workspace, 5"
-        "$mainMod, 6, workspace, 6"
-        "$mainMod, 7, workspace, 7"
-        "$mainMod, 8, workspace, 8"
-        "$mainMod, 9, workspace, 9"
-        "$mainMod, 0, workspace, 10"
+        "$mainMod, 1, focusworkspaceoncurrentmonitor, 1"
+        "$mainMod, 2, focusworkspaceoncurrentmonitor, 2"
+        "$mainMod, 3, focusworkspaceoncurrentmonitor, 3"
+        "$mainMod, 4, focusworkspaceoncurrentmonitor, 4"
+        "$mainMod, 5, focusworkspaceoncurrentmonitor, 5"
+        "$mainMod, 6, focusworkspaceoncurrentmonitor, 6"
+        "$mainMod, 7, focusworkspaceoncurrentmonitor, 7"
+        "$mainMod, 8, focusworkspaceoncurrentmonitor, 8"
+        "$mainMod, 9, focusworkspaceoncurrentmonitor, 9"
+        "$mainMod, 0, focusworkspaceoncurrentmonitor, 10"
 
         "$mainMod CONTROL, H, workspace, e-1"
         "$mainMod CONTROL, L, workspace, e+1"
