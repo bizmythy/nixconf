aec9ca884eb00f1d61003e2d2afc4a223e3f6c79
drew-dirac <drew@diracinc.com>
2025-03-07 15:46:52 -0500

    add dirac corsair pc conf

--- flake.nix
+++ flake.nix
@@ -71,6 +71,7 @@
             "igneous"
             "theseus"
             "drewdirac"
+            "drewdiracpc"
           ]
       );
     };
---
+++ hosts/drewdiracpc/configuration.nix
@@ -0,0 +1,29 @@
+{
+  ...
+}:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  config = {
+    # enable customizations for nvidia
+    nvidiaEnable = true;
+
+    # specify the GPU IDs, set to offload mode
+    # hardware.nvidia.prime = {
+    #   offload.enable = true;
+    #   intelBusId = "PCI:0:2:0";
+    #   nvidiaBusId = "PCI:1:0:0";
+    # };
+
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It‘s perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "24.11"; # Did you read the comment?
+  };
+}
