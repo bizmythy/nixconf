b151f76b71615e15a41891ed4357a53d4ea34fb3
AndrewCouncil <andrew.p.council@gmail.com>
2025-03-13 15:39:02 -0400

    configurating ssh for server, varying results

--- home/ssh.nix
+++ home/ssh.nix
@@ -6,6 +6,11 @@
 let
   # onePassPath = "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
   onePassPath = "~/.1password/agent.sock";
+
+  diraclocalserverIdentityFile = pkgs.writeTextFile {
+    name = "diraclocalserver.pub";
+    text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEoZXGhcmj8ZUFPWWGw3fZAd0FOCZKXnKelZKaGD9Tq4";
+  };
 in
 {
   # home manager version adds several extra options i do not want
@@ -20,5 +25,23 @@ in
   home.file.".ssh/config".text = ''
     Host *
         IdentityAgent ${onePassPath}
+
+    Host diraclocalserver
+        HostName 192.168.1.154
+        User diraclocalserver
+        IdentityAgent ${onePassPath}
+        IdentityFile ${diraclocalserverIdentityFile}
+        IdentitiesOnly yes
+  '';
+
+  xdg.configFile."1Password/ssh/agent.toml".text = ''
+    [[ssh-keys]]
+    vault = "Private"
+
+    [[ssh-keys]]
+    vault = "Employee"
+
+    [[ssh-keys]]
+    vault = "Engineering"
   '';
 }
