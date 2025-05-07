333da4e2b72b5a8b17958c08c1b178b0ae0b3c50
AndrewCouncil <andrew.p.council@gmail.com>
2025-05-07 11:27:51 -0400

    add nushell syntax to bat

--- home/home.nix
+++ home/home.nix
@@ -76,6 +76,12 @@ in
         style = "header-filename,rule,snip";
         color = "always";
       };
+      syntaxes = {
+        nushell.src = pkgs.fetchurl {
+          url = "https://gist.githubusercontent.com/melMass/294c21a113d0bd329ae935a79879fe04/raw/nushell.sublime-syntax";
+          hash = "sha256-QSjnGrv3o9qZ74b6Hk6pXJ6fx2Dq8U0cu9fyd51zokw=";
+        };
+      };
     };
 
     chromium = {
