4640d0fc53d599e9ad665c51830f599a429664a0
AndrewCouncil <andrew.p.council@gmail.com>
2025-05-18 14:51:26 -0400

    add service to auto log out when a work machine

--- home/wm/hypr/hyprlock.nix
+++ home/wm/hypr/hyprlock.nix
@@ -5,9 +5,32 @@
   vars,
   ...
 }:
+let
+  isWork = builtins.elem osConfig.networking.hostname [
+    "drewdirac"
+    "drewdiracpc"
+  ];
+  lockScreenTimeout = 60 / 2; # inactive seconds before lock screen
+in
 {
   programs.hyprlock = {
     enable = true;
     # settings = {};
+  };
+  services.hypridle = lib.mkIf isWork {
+    enable = true;
+    settings = {
+      general = {
+        lock_cmd = "pidof hyprlock || hyprlock"; # avoid starting multiple hyprlock instances.
+        before_sleep_cmd = "loginctl lock-session"; # lock before suspend.
+        after_sleep_cmd = "hyprctl dispatch dpms on"; # to avoid having to press a key twice to turn on the display.
+      };
+      listener = [
+        {
+          timeout = lockScreenTimeout;
+          on-timeout = "loginctl lock-session"; # lock screen when timeout has passed
+        }
+      ];
+    };
   };
 }
