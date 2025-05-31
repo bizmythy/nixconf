1b82c41635f76a24937451f77120d92029bc88ed
AndrewCouncil <andrew.p.council@gmail.com>
2025-05-31 15:46:25 -0400

    subtler bloom shader for ghostty

--- home/tty.nix
+++ home/tty.nix
@@ -52,40 +52,53 @@ in
     };
   };
 
-  programs.ghostty = {
-    enable = true;
-    settings = {
-      command = startCommand;
-      gtk-single-instance = true;
+  programs.ghostty =
+    let
+      bloomAmmount = "0.04";
+      ghosttyShaders = pkgs.stdenv.mkDerivation {
+        name = "bloom-shader";
+        src = pkgs.fetchFromGitHub {
+          owner = "hackr-sh";
+          repo = "ghostty-shaders";
+          rev = "a17573fb254e618f92a75afe80faa31fd5e09d6f";
+          hash = "sha256-p0speO5BtLZZwGeuRvBFETnHspDYg2r5Uiu0yeqj1iE=";
+        };
 
-      font-family = fontFamily;
-      font-size = fontSize;
-      background-opacity = backgroundOpacity;
-      cursor-style = "bar";
-      cursor-style-blink = false;
-      adjust-cursor-thickness = 2;
-      shell-integration-features = "no-cursor";
+        postPatch = ''
+          substituteInPlace bloom.glsl --replace-fail "0.2;" "${bloomAmmount};"
+        '';
 
-      copy-on-select = "clipboard";
-      app-notifications = "no-clipboard-copy";
-      confirm-close-surface = false;
-      link-url = true;
+        installPhase = ''
+          mkdir -p $out
+          cp -R ./* $out/
+        '';
+      };
+    in
+    {
+      enable = true;
+      settings = {
+        command = startCommand;
+        gtk-single-instance = true;
 
-      # custom-shader =
-      #   let
-      #     ghostty-shaders = pkgs.fetchFromGitHub {
-      #       owner = "hackr-sh";
-      #       repo = "ghostty-shaders";
-      #       rev = "a17573fb254e618f92a75afe80faa31fd5e09d6f";
-      #       hash = "sha256-p0speO5BtLZZwGeuRvBFETnHspDYg2r5Uiu0yeqj1iE=";
-      #     };
-      #   in
-      #   "${ghostty-shaders}/bloom.glsl";
+        font-family = fontFamily;
+        font-size = fontSize;
+        background-opacity = backgroundOpacity;
+        cursor-style = "bar";
+        cursor-style-blink = false;
+        adjust-cursor-thickness = 2;
+        shell-integration-features = "no-cursor";
+
+        copy-on-select = "clipboard";
+        app-notifications = "no-clipboard-copy";
+        confirm-close-surface = false;
+        link-url = true;
+
+        custom-shader = "${ghosttyShaders}/bloom.glsl";
+      };
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+      enableFishIntegration = true;
+      installBatSyntax = true;
+      package = pkgs.ghostty;
     };
-    enableBashIntegration = true;
-    enableZshIntegration = true;
-    enableFishIntegration = true;
-    installBatSyntax = true;
-    package = pkgs.ghostty;
-  };
 }
