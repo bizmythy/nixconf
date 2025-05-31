def1b784b59be1603c5cc9d5d3b6daf58e209326
AndrewCouncil <andrew.p.council@gmail.com>
2025-05-31 15:12:10 -0400

    add docs, desktop shortcut for launch with shaders

--- home/spotify-player.nix
+++ home/spotify-player.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   osConfig,
 }:
 {
@@ -16,9 +17,13 @@
           "op://smfpz5gfxgz5meqdqachl7lqw4/spotify-player/credential"
         ];
       };
+
       device.name = "${osConfig.networking.hostName}-spotify-player";
+
+      # weird values needed to make album art square
       cover_img_width = 9;
       cover_img_length = 20;
+
       border_type = "Rounded";
       layout = {
         playback_window_position = "Bottom";
@@ -82,4 +87,25 @@
       }
     ];
   };
+
+  # create desktop shortcut for launching
+  xdg.desktopEntries.spotify-player =
+    let
+      ghostty-shaders = pkgs.fetchFromGitHub {
+        owner = "hackr-sh";
+        repo = "ghostty-shaders";
+        rev = "a17573fb254e618f92a75afe80faa31fd5e09d6f";
+        hash = "sha256-p0speO5BtLZZwGeuRvBFETnHspDYg2r5Uiu0yeqj1iE=";
+      };
+    in
+    {
+      name = "spotify-player";
+      exec = "ghostty --custom-shader=${ghostty-shaders}/bloom.glsl -e spotify_player";
+      terminal = false;
+      type = "Application";
+      categories = [
+        "Audio"
+        "Music"
+      ];
+    };
 }
