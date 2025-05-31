90e0da0ddd760b729fbf82b88d3fd34ec0a9d0c4
AndrewCouncil <andrew.p.council@gmail.com>
2025-05-31 17:42:44 -0400

    ai translation of json waybar to nix

--- home/wm/bar/waybar.nix
+++ home/wm/bar/waybar.nix
@@ -11,10 +11,241 @@
   programs.waybar = {
     enable = true;
     style = ./waybar.css;
+    settings = {
+      mainBar = {
+        height = 10;
+        spacing = 4;
+
+        modules-left = [
+          "hyprland/workspaces"
+          "custom/media"
+        ];
+        modules-center = [
+          "hyprland/window"
+        ];
+        modules-right = [
+          "mpd"
+          "pulseaudio"
+          "battery"
+          "battery#bat2"
+          "tray"
+          "custom/notification"
+          "clock"
+          "custom/power"
+        ];
+
+        "keyboard-state" = {
+          numlock = true;
+          capslock = true;
+          format = "{name} {icon}";
+          format-icons = {
+            locked = "";
+            unlocked = "";
+          };
+        };
+
+        mpd = {
+          format = "{stateIcon} {consumeIcon}{randomIcon}{repeatIcon}{singleIcon}{artist} - {album} - {title} ({elapsedTime:%M:%S}/{totalTime:%M:%S}) ⸨{songPosition}|{queueLength}⸩ {volume}% ";
+          format-disconnected = "Disconnected ";
+          format-stopped = "{consumeIcon}{randomIcon}{repeatIcon}{singleIcon}Stopped ";
+          unknown-tag = "N/A";
+          interval = 5;
+          consume-icons = {
+            "on" = " ";
+          };
+          random-icons = {
+            "off" = "<span color=\"#f53c3c\"></span> ";
+            "on" = " ";
+          };
+          repeat-icons = {
+            "on" = " ";
+          };
+          single-icons = {
+            "on" = "1 ";
+          };
+          state-icons = {
+            paused = "";
+            playing = "";
+          };
+          tooltip-format = "MPD (connected)";
+          tooltip-format-disconnected = "MPD (disconnected)";
+        };
+
+        idle_inhibitor = {
+          format = "{icon}";
+          format-icons = {
+            activated = "";
+            deactivated = "";
+          };
+        };
+
+        tray = {
+          spacing = 10;
+        };
+
+        clock = {
+          format = "{:%I:%M}";
+          tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+          format-alt = "{:%Y-%m-%d}";
+        };
+
+        cpu = {
+          format = "{usage}% ";
+          tooltip = false;
+        };
+
+        memory = {
+          format = "{}% ";
+        };
+
+        temperature = {
+          critical-threshold = 80;
+          format = "{temperatureC}°C {icon}";
+          format-icons = [
+            ""
+            ""
+            ""
+          ];
+        };
+
+        backlight = {
+          format = "{percent}% {icon}";
+          format-icons = [
+            ""
+            ""
+            ""
+            ""
+            ""
+            ""
+            ""
+            ""
+            ""
+          ];
+        };
+
+        battery = {
+          states = {
+            warning = 30;
+            critical = 15;
+          };
+          format = "{capacity}% {icon}";
+          format-full = "{capacity}% {icon}";
+          format-charging = "{capacity}% ";
+          format-plugged = "{capacity}% ";
+          format-alt = "{time} {icon}";
+          format-icons = [
+            ""
+            ""
+            ""
+            ""
+            ""
+          ];
+        };
+
+        "battery#bat2" = {
+          bat = "BAT2";
+        };
+
+        power-profiles-daemon = {
+          format = "{icon}";
+          tooltip-format = "Power profile: {profile}\nDriver: {driver}";
+          tooltip = true;
+          format-icons = {
+            "default" = "";
+            performance = "";
+            balanced = "";
+            power-saver = "";
+          };
+        };
+
+        network = {
+          format-wifi = "{essid} ";
+          format-ethernet = "{ipaddr}/{cidr} ";
+          tooltip-format = "{ifname} via {gwaddr}";
+          format-linked = "{ifname} (No IP) ";
+          format-disconnected = "Disconnected ⚠";
+          on-click = "nm-connection-editor";
+        };
+
+        bluetooth = {
+          format = " {status}";
+          format-disabled = "";
+          format-connected = " {num_connections}";
+          tooltip-format = "{device_alias}";
+          tooltip-format-connected = " {device_enumerate}";
+          tooltip-format-enumerate-connected = "{device_alias}";
+          on-click = "blueman-manager";
+        };
+
+        pulseaudio = {
+          format = "{volume}% {icon} {format_source}";
+          format-bluetooth = "{volume}% {icon} {format_source}";
+          format-source = "";
+          format-source-muted = "";
+          format-icons = {
+            headphone = "";
+            phone = "";
+            portable = "";
+            car = "";
+            "default" = [
+              ""
+              ""
+              ""
+            ];
+          };
+          on-click = "pavucontrol";
+        };
+
+        "custom/media" = {
+          format = "{icon} {text}";
+          return-type = "json";
+          max-length = 40;
+          format-icons = {
+            spotify = "";
+            "default" = "🎜";
+          };
+          escape = true;
+          exec = "$HOME/.config/waybar/mediaplayer.py 2> /dev/null";
+        };
+
+        "custom/notification" = {
+          tooltip = false;
+          format = "{icon}";
+          format-icons = {
+            notification = "<span foreground='red'><sup></sup></span> ";
+            none = " ";
+            dnd-notification = "<span foreground='red'><sup></sup></span> ";
+            dnd-none = " ";
+            inhibited-notification = "<span foreground='red'><sup></sup></span> ";
+            inhibited-none = " ";
+            dnd-inhibited-notification = "<span foreground='red'><sup></sup></span> ";
+            dnd-inhibited-none = " ";
+          };
+          return-type = "json";
+          exec-if = "which swaync-client";
+          exec = "swaync-client -swb";
+          on-click = "swaync-client -t -sw";
+          on-click-right = "swaync-client -d -sw";
+          escape = true;
+        };
+
+        "custom/power" = {
+          format = "⏻ ";
+          tooltip = false;
+          menu = "on-click";
+          menu-file = "${./power_menu.xml}";
+          menu-actions = {
+            shutdown = "poweroff";
+            reboot = "reboot";
+            suspend = "systemctl suspend";
+            hibernate = "systemctl hibernate";
+          };
+        };
+      };
+    };
   };
 
-  xdg.configFile = {
-    "waybar/config.jsonc".source = ./config.jsonc;
-    "waybar/power_menu.xml".source = ./power_menu.xml;
-  };
+  # xdg.configFile = {
+  #   "waybar/power_menu.xml".source = ./power_menu.xml;
+  # };
 }
