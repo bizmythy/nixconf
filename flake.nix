c2452eb4b00b8d00bee44c48502df3746bcf70b7
AndrewCouncil <andrew.p.council@gmail.com>
2025-07-04 12:49:44 -0400

    switch to new zen browser flake with udpated version

--- flake.nix
+++ flake.nix
@@ -15,7 +15,10 @@
 
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
 
-    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    zen-browser = {
+      url = "github:youwen5/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     dirac = {
       type = "git";
