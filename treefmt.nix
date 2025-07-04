67a44857d2d3d99cbd2a933620844235bfa3af9a
AndrewCouncil <andrew.p.council@gmail.com>
2025-07-04 12:19:44 -0400

    use prettier and shellcheck

--- treefmt.nix
+++ treefmt.nix
@@ -5,6 +5,8 @@
 {
   projectRootFile = "flake.nix";
   programs.nixfmt.enable = true;
+  programs.prettier.enable = true;
+  programs.shellcheck.enable = true;
   # buggy as of right now
   # programs.nufmt.enable = true;
 }
