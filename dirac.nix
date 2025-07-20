a245e2e272a79765dd7e3cdd05e9f9bc492f71bd
AndrewCouncil <andrew.p.council@gmail.com>
2025-07-20 14:08:10 -0400

    try don't autostart twingate

--- dirac.nix
+++ dirac.nix
@@ -3,11 +3,16 @@
   ...
 }:
 {
+  # don't get the default graphical packages, i will manage them myself
   dirac.graphical = false;
+
   # override what i am already managing in home manager
   programs = {
     direnv.enable = false;
     git.enable = lib.mkForce false;
     starship.enable = false;
   };
+
+  # don't start twingate at boot, will be started manually when desired
+  systemd.services."twingate".wantedBy = lib.mkForce [ ];
 }
