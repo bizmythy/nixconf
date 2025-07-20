874b30ff64da41bc1bf3799eaebfec3cfa1cb895
AndrewCouncil <andrew.p.council@gmail.com>
2025-07-20 13:45:50 -0400

    switch to usnig jsonfmt, don't need prettier

--- treefmt.nix
+++ treefmt.nix
@@ -6,7 +6,7 @@
 {
   projectRootFile = "flake.nix";
   programs.nixfmt.enable = true;
-  programs.prettier.enable = true;
+  programs.jsonfmt.enable = true;
   programs.shellcheck.enable = true;
 
   # buggy as of right now
