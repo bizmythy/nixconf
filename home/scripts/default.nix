d1d610b3b75ba66ec7dd2a1f68e1fda1e96eb05c
AndrewCouncil <andrew.p.council@gmail.com>
2025-07-27 10:53:45 -0400

    try making nice function to map over script files

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -1,9 +1,15 @@
 {
   pkgs,
+  lib,
   ...
 }:
+let
+  # make a script with binary name based on script name
+  getName = path: builtins.elemAt (lib.splitString "." (builtins.baseNameOf path)) 0;
+  makeScript = path: pkgs.writeScriptBin (getName path) (builtins.readFile path);
+in
 {
-  home.packages = with pkgs; [
-    (writeScriptBin "flakeup" (builtins.readFile ./flakeup.nu))
+  home.packages = map makeScript [
+    ./flakeup.nu
   ];
 }
