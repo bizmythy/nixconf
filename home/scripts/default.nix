ca51ed383a3b5bc885bea2afcf0463606ef7473d
bizmythy <andrew.p.council@gmail.com>
2025-08-13 20:21:55 -0400

    automatically import scripts from the src directory

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -4,13 +4,15 @@
   ...
 }:
 let
+  # import all files in the ./src directory as scripts
+  scriptDir = ./src;
+  scriptFileNames = builtins.attrNames (builtins.readDir scriptDir);
+  scriptFilePaths = map (path: scriptDir + "/${path}") scriptFileNames;
+
   # make a script with binary name based on script name
   getName = path: builtins.elemAt (lib.splitString "." (builtins.baseNameOf path)) 0;
   makeScript = path: pkgs.writeScriptBin (getName path) (builtins.readFile path);
 in
 {
-  home.packages = map makeScript [
-    ./flakeup.nu
-    ./switchaudio.nu
-  ];
+  home.packages = map makeScript scriptFilePaths;
 }
