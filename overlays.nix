8f5c5b77c0f16c738fb5a3a80ba3849d7fb378d7
bizmythy <andrew.p.council@gmail.com>
2025-09-15 16:09:01 -0400

    Reapply "temp disable 1password override"
    This reverts commit 09c3a57ed2bfdb4448c848b76880b4cb19ce0daa.

--- overlays.nix
+++ overlays.nix
@@ -33,25 +33,25 @@ self: super: {
     }
   );
 
-  _1password-gui = super._1password-gui.overrideAttrs (
-    finalAttrs: previousAttrs: {
-      preFixup = ''
-        # makeWrapper defaults to makeBinaryWrapper due to wrapGAppsHook
-        # but we need a shell wrapper specifically for `NIXOS_OZONE_WL`.
-        # Electron is trying to open udev via dlopen()
-        # and for some reason that doesn't seem to be impacted from the rpath.
-        # Adding udev to LD_LIBRARY_PATH fixes that.
-        # Make xdg-open overrideable at runtime.
-        makeShellWrapper $out/share/1password/1password $out/bin/1password \
-          "''${gappsWrapperArgs[@]}" \
-          --suffix PATH : ${super.lib.makeBinPath [ super.xdg-utils ]} \
-          --prefix LD_LIBRARY_PATH : ${super.lib.makeLibraryPath [ super.udev ]}
-          # Currently half broken on wayland (e.g. no copy functionality)
-          # See: https://github.com/NixOS/nixpkgs/pull/232718#issuecomment-1582123406
-          # Remove this comment when upstream fixes:
-          # https://1password.community/discussion/comment/624011/#Comment_624011
-          #--add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}}"
-      '';
-    }
-  );
+  # _1password-gui = super._1password-gui.overrideAttrs (
+  #   finalAttrs: previousAttrs: {
+  #     preFixup = ''
+  #       # makeWrapper defaults to makeBinaryWrapper due to wrapGAppsHook
+  #       # but we need a shell wrapper specifically for `NIXOS_OZONE_WL`.
+  #       # Electron is trying to open udev via dlopen()
+  #       # and for some reason that doesn't seem to be impacted from the rpath.
+  #       # Adding udev to LD_LIBRARY_PATH fixes that.
+  #       # Make xdg-open overrideable at runtime.
+  #       makeShellWrapper $out/share/1password/1password $out/bin/1password \
+  #         "''${gappsWrapperArgs[@]}" \
+  #         --suffix PATH : ${super.lib.makeBinPath [ super.xdg-utils ]} \
+  #         --prefix LD_LIBRARY_PATH : ${super.lib.makeLibraryPath [ super.udev ]}
+  #         # Currently half broken on wayland (e.g. no copy functionality)
+  #         # See: https://github.com/NixOS/nixpkgs/pull/232718#issuecomment-1582123406
+  #         # Remove this comment when upstream fixes:
+  #         # https://1password.community/discussion/comment/624011/#Comment_624011
+  #         #--add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}}"
+  #     '';
+  #   }
+  # );
 }
