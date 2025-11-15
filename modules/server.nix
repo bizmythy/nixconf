918e0987b66f5fc511fc971a0f710612fa0c1a82
bizmythy <andrew.p.council@gmail.com>
2025-11-15 12:06:54 -0500

    create server.nix entrypoint for servers

---
+++ modules/server.nix
@@ -0,0 +1,152 @@
+{
+  config,
+  inputs,
+  lib,
+  pkgs,
+  vars,
+  ...
+}:
+
+{
+  imports = [
+    inputs.home-manager.nixosModules.default
+    ./nvidia.nix
+    ./packages/terminal.nix
+  ];
+
+  boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
+
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+
+    # cachix for hyprland flake and dirac
+    substituters = [
+      "https://numtide.cachix.org"
+      "https://hyprland.cachix.org"
+    ];
+    trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+    ];
+  };
+
+  programs.nix-ld = {
+    enable = true;
+    # libraries = with pkgs; [
+    #   # Add any missing dynamic libraries for unpackaged
+    #   # programs here, NOT in environment.systemPackages
+    # ];
+  };
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
+  };
+
+  services.clamav = {
+    daemon.enable = false;
+    scanner.enable = false;
+    updater.enable = false;
+  };
+
+  # use sudo-rs instead of default sudo
+  security = {
+    sudo.enable = false;
+    sudo-rs = {
+      enable = true;
+      execWheelOnly = true;
+      wheelNeedsPassword = true;
+    };
+  };
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = false;
+  services.pipewire.enable = false;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users."${vars.user}" = {
+    isNormalUser = true;
+    description = vars.user;
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "docker"
+      "cdrom"
+    ];
+    # packages = with pkgs; [
+    # ];
+  };
+
+  # Remap CAPS lock to ESC
+  # TODO: find more nixos-way to do this, maybe use Kanata?
+  services.udev.extraHwdb = ''
+    evdev:atkbd:*
+      KEYBOARD_KEY_3a=esc
+  '';
+
+  # Mount NAS, optional for boot
+  fileSystems."/mnt/tungsten-vault" = lib.mkIf (vars.isPersonal config) {
+    device = "192.168.1.202:/mnt/tungsten/tungsten-vault";
+    fsType = "nfs";
+    options = [
+      "x-systemd.automount"
+      "noauto"
+    ];
+  };
+
+  # Enable SSH
+  services.openssh = {
+    enable = true;
+    ports = [ 22 ];
+    settings = {
+      PasswordAuthentication = false;
+      AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
+      UseDns = true;
+      X11Forwarding = false;
+      PermitRootLogin = "prohibit-password";
+    };
+  };
+
+  # add pocl to always have cpu opencl support at minimum
+  hardware.graphics.extraPackages = with pkgs; [ pocl ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = true;
+}
