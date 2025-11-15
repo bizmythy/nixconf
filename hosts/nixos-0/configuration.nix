953d72da94ab6c2e86ae9b39f9a5a564f126275c
bizmythy <andrew.p.council@gmail.com>
2025-11-15 14:38:15 -0500

    add to ssh key to drew account

--- hosts/nixos-0/configuration.nix
+++ hosts/nixos-0/configuration.nix
@@ -1,4 +1,7 @@
-{ ... }:
+{
+  vars,
+  ...
+}:
 {
   imports = [
     ./hardware-configuration.nix
@@ -11,6 +14,9 @@
   networking.domain = "";
   services.openssh.enable = true;
   users.users.root.openssh.authorizedKeys.keys = [
+    ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGogIJ4uaReEMnM8eRedZh0OVq/4AAs4H8xdiWjvf6YF''
+  ];
+  users.users."${vars.user}".openssh.authorizedKeys.keys = [
     ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGogIJ4uaReEMnM8eRedZh0OVq/4AAs4H8xdiWjvf6YF''
   ];
   system.stateVersion = "23.11";
