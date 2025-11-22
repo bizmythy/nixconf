f3581efd37244ced98f2a83f42bfc3a594adc885
bizmythy <andrew.p.council@gmail.com>
2025-11-21 20:11:19 -0500

    configure ftbskies mc server

--- hosts/nixos-0/containers.nix
+++ hosts/nixos-0/containers.nix
@@ -1,6 +1,36 @@
 {
+  vars,
   ...
 }:
 {
-  virtualisation.oci-containers = {};
+  virtualisation.oci-containers =
+    let
+      minecraftServerImg = "itzg/minecraft-server:java8-multiarch";
+      portDef = { host, container }: "${builtins.toString host}:${builtins.toString container}";
+    in
+    {
+      backend = "podman";
+      containers = {
+        ftbskies = {
+          image = minecraftServerImg;
+          environment = {
+            EULA = "TRUE";
+            TYPE = "FTBA";
+            FTB_MODPACK_ID = 129;
+            FTB_MODPACK_VERSION_ID = 100154;
+          };
+
+          volumes = [ "/home/drew/minecraft/ftbskies:/data" ];
+
+          user = vars.user;
+          autoStart = true;
+          ports = map portDef [
+            {
+              host = 25565;
+              container = 25565;
+            }
+          ];
+        };
+      };
+    };
 }
