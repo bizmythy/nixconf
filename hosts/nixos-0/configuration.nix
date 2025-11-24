2b0075bb45459c283d3e146b44dccd58f4f4c509
bizmythy <andrew.p.council@gmail.com>
2025-11-24 18:12:42 -0500

    add as systemd timer event

--- hosts/nixos-0/configuration.nix
+++ hosts/nixos-0/configuration.nix
@@ -1,7 +1,10 @@
 {
   vars,
+  pkgs,
+  lib,
   ...
 }:
+
 {
   imports = [
     ./hardware-configuration.nix
@@ -32,5 +35,32 @@
       root = authorizedKeys;
       "${vars.user}" = authorizedKeys;
     };
+
+  systemd.services.serverbackup =
+    let
+      serverBackupScript = pkgs.writers.writeNuBin "serverbackup" {
+        runtimeInputs = with pkgs; [
+          rclone
+          gnutar
+          zstd
+        ];
+      } (builtins.readFile ./serverbackup.nu);
+    in
+    {
+      description = "Weekly Minecraft backup to pCloud";
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = lib.getExe serverBackupScript;
+      };
+    };
+
+  systemd.timers.serverbackup = {
+    description = "Run serverbackup weekly on Wednesdays at 12:00";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "Wed 12:00"; # weekly, noon on Wednesday
+      Persistent = true; # catch up if missed
+    };
+  };
   system.stateVersion = "23.11";
 }
