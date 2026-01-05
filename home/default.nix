ebf31650da0be34e79fcf95a666479a9a57225c6
AndrewCouncil <andrew.p.council@gmail.com>
2026-01-05 12:17:13 -0500

    formatting change, add cargo install bins to path

--- home/default.nix
+++ home/default.nix
@@ -20,22 +20,28 @@
     # keep-sorted end
   ];
 
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = vars.user;
-  home.homeDirectory = vars.home;
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
-
-  # home.packages = with pkgs; [
-  # ];
+  home = let
+    homeDirectory = vars.home;
+  in
+  {
+    # Home Manager needs a bit of information about you and the paths it should
+    # manage.
+    username = vars.user;
+    inherit homeDirectory;
+  
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager release
+    # introduces backwards incompatible changes.
+    #
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    stateVersion = "24.11"; # Please read the comment before changing.
+    
+    sessionPath = [
+      "${homeDirectory}/.cargo/bin" # programs from `cargo install`
+    ];
+  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
