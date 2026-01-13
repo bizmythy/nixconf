fa66dacea5bc58717325623e4999ed05531ab5fc
bizmythy <andrew.p.council@gmail.com>
2026-01-13 16:17:23 -0500

    formatting

--- hosts/drewdiracframework/configuration.nix
+++ hosts/drewdiracframework/configuration.nix
@@ -16,7 +16,7 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11"; # Did you read the comment?
-  
+
   hardware.graphics.extraPackages = with pkgs; [ rocmPackages.clr.icd ];
 
   environment.systemPackages = with pkgs; [
