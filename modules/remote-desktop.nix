b19c8b0d2f332326a2283292787fb7d2023b5459
bizmythy <andrew.p.council@gmail.com>
2026-02-27 19:11:21 -0500

    TEMP: sunshine won't build

--- modules/remote-desktop.nix
+++ modules/remote-desktop.nix
@@ -8,7 +8,7 @@
   config = lib.mkIf (vars.isPersonal config) {
     # low-latency desktop/game streaming
     services.sunshine = {
-      enable = true;
+      enable = false; # TEMP
       autoStart = false;
       openFirewall = true;
     };
