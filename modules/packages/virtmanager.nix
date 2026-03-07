20e02d863528a170e211d554d21cbf2bfe6affa1
bizmythy <andrew.p.council@gmail.com>
2026-03-07 15:34:37 -0500

    TEMP: override to fix bug with virt manager service

--- modules/packages/virtmanager.nix
+++ modules/packages/virtmanager.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   vars,
   ...
 }:
@@ -6,6 +7,16 @@
   programs.virt-manager.enable = true;
 
   users.groups.libvirtd.members = [ vars.user ];
+  # Temporary workaround for libvirt 12.1.0 shipping a systemd unit that
+  # hardcodes /usr/bin/sh, which does not exist on NixOS.
+  systemd.services.virt-secret-init-encryption = {
+    overrideStrategy = "asDropin";
+    serviceConfig.ExecStart = [
+      ""
+      "${pkgs.runtimeShell} -c 'umask 0077 && (${pkgs.coreutils}/bin/dd if=/dev/random status=none bs=32 count=1 | ${pkgs.systemd}/bin/systemd-creds encrypt --name=secrets-encryption-key - /var/lib/libvirt/secrets/secrets-encryption-key)'"
+    ];
+  };
+
   virtualisation = {
     libvirtd = {
       enable = true;
