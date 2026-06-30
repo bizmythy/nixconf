3fd2ca4e8d0dff3c6e31c7d171f2b31fe1de9c63
bizmythy <andrew.p.council@gmail.com>
2026-06-30 17:45:43 -0400

    temp: track stable version of vesktop

--- home/programs/vesktop.nix
+++ home/programs/vesktop.nix
@@ -1,11 +1,21 @@
 {
+  inputs,
   lib,
   osConfig,
+  pkgs,
   vars,
   ...
 }:
+
+let
+  stablePkgs = import inputs.nixpkgs-stable {
+    inherit (pkgs.stdenv.hostPlatform) system;
+    config.allowUnfree = true;
+  };
+in
 {
   programs.vesktop = lib.mkIf (vars.isPersonal osConfig) {
     enable = true;
+    package = stablePkgs.vesktop;
   };
 }
