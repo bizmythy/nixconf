617327239e4be9dd71bf620f579cc65051c9aa62
bizmythy <andrew.p.council@gmail.com>
2026-07-02 12:30:03 -0400

    Revert "add `tuicr`"
    This reverts commit ad8de21ad02e429f207c5769fb028e5326fd5591.

--- modules/packages/terminal.nix
+++ modules/packages/terminal.nix
@@ -97,7 +97,6 @@
         presenterm # terminal presentations
         screen # run commands in background
         glow # view markdown in terminal
-        tuicr # github PR review in terminal
         (aspellWithDicts (ps: with ps; [ en ])) # spell check text files
 
         graphite-cli
