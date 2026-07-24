032795b0ce60e25ab0a0243aaf3cadfd54ba5e1f
bizmythy <andrew.p.council@gmail.com>
2026-07-24 12:04:52 -0400

    remove here as well

--- dirac.nix
+++ dirac.nix
@@ -7,11 +7,6 @@
   # don't get the default graphical packages, i will manage them myself
   dirac.graphical = false;
 
-  services.dirac-workspaces = {
-    enable = false;
-    user = vars.user;
-  };
-
   systemd.user.services.dirac-workspacesd.environment.SSH_AUTH_SOCK = "%h/.1password/agent.sock";
 
   # override what i am already managing in home manager
