d227974d5550210750081f1a7008596062e60eee
bizmythy <andrew.p.council@gmail.com>
2026-07-24 11:58:34 -0400

    stop using stable?

--- home/programs/vesktop.nix
+++ home/programs/vesktop.nix
@@ -7,16 +7,9 @@
   ...
 }:
 
-let
-  stablePkgs = import inputs.nixpkgs-stable {
-    inherit (pkgs.stdenv.hostPlatform) system;
-    config.allowUnfree = true;
-  };
-in
 {
   programs.vesktop = lib.mkIf (vars.isPersonal osConfig) {
     enable = true;
-    package = stablePkgs.vesktop;
     # Ad-block custom CSS, should hopefully stay up-to-date with discord ads...
     vencord.extraQuickCss = lib.mkAfter ''
       @import url(https://codeberg.org/ridge/Discord-Adblock/raw/branch/main/discord-adblock.css);
