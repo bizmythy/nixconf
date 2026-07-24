7a72c751c00cefec8e82532de0ccacf805eeace8
bizmythy <andrew.p.council@gmail.com>
2026-07-24 11:57:06 -0400

    add ad-block custom CSS for vesktop

--- home/programs/vesktop.nix
+++ home/programs/vesktop.nix
@@ -17,5 +17,9 @@ in
   programs.vesktop = lib.mkIf (vars.isPersonal osConfig) {
     enable = true;
     package = stablePkgs.vesktop;
+    # Ad-block custom CSS, should hopefully stay up-to-date with discord ads...
+    vencord.extraQuickCss = lib.mkAfter ''
+      @import url(https://codeberg.org/ridge/Discord-Adblock/raw/branch/main/discord-adblock.css);
+    '';
   };
 }
